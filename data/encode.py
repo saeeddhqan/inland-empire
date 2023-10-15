4cc021bd614d8dae24c669e3d3f4cfebdc243079
saeeddhqan <saeeddhqan@gmail.com>
2023-10-15 12:12:40 +0330

    next

---
+++ data/encode.py
@@ -0,0 +1,42 @@
+from collections import Counter
+import numpy
+import torch
+
+text = open('politic_50k.txt').read()
+# text2 = open('politic_1k.txt').read()
+
+cnt = Counter(text.split(' '))
+chars = sorted(list(set(text)))
+top50k = [x[0] for x in cnt.most_common(20000)]
+stoi = {top50k[x]: x for x in range(len(top50k))}
+for x in range(len(chars)):
+	c = chars[x]
+	if c not in stoi:
+		stoi[c] = len(stoi)
+itos = {i:c for c,i in stoi.items()}
+
+encode = lambda s: [stoi[x] for x in s]
+decode = lambda s: ''.join([itos[x.item()] for x in s])
+
+
+def encode():
+	seq = []
+	for doc in text.split('\n'):
+		if doc == '':
+			continue
+		for token in doc.split():
+			if token in stoi:
+				seq.append(stoi[token])
+			else:
+				for c in token:
+					seq.append(stoi[c])
+			seq.append(stoi[' '])
+		seq.append(stoi['\n'])
+	numpy.save('politic_50k.npy', numpy.array(seq))
+
+encode()
+
+loaded_array = torch.from_numpy(numpy.load('politic_50k.npy'))
+
+print(loaded_array.shape)
+print(decode(loaded_array[10:1000]))
